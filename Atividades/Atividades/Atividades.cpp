8c51ba1481be39bae981baa343cb21b05e6f3aaa
jefferosa <jefmi.98@gmail.com>
2021-10-18 21:20:32 -0300

    Inicio atividades
    Java JUnit

---
+++ Atividades/Atividades/Atividades.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+
+using namespace std;
+typedef long long ll;
+int gcd(int a, int b) { return (b == 0 ? a : gcd(b, a % b)); }
+
+int main() 
+{
+	int8_t a = 200;
+	uint8_t b = 100;
+
+	cout << (int)a << (int)b;
+
+	return 0;
+}
---
+++ TreinoA/TreinoA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TreinoA.MvcApplication" Language="C#" %>
