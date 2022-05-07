52212a8c230abdbbf0f2965676380222e94fb96f
jefferosa <jefmi.98@gmail.com>
2022-05-07 16:37:36 -0300

    Iinicio MiniZelda

---
+++ zeldaminiclone/src/zeldaminiclone/World.java
@@ -0,0 +1,25 @@
+package zeldaminiclone;
+
+import java.awt.Graphics;
+import java.util.List;
+import java.util.ArrayList;
+
+public class World 
+{
+	public List<Blocks> blocos = new ArrayList<Blocks>();
+	
+	public World()
+	{
+		for (int xx = 0; xx < 15; xx++)
+			blocos.add(new Blocks(xx * 32, 0));
+		
+		for (int xx = 0; xx < 15; xx++)
+			blocos.add(new Blocks(xx * 32, 480 - 32));
+	}
+	
+	public void render(Graphics g)
+	{
+		for (int i = 0; i < blocos.size(); i++)
+			blocos.get(i).render(g);
+	}
+}
